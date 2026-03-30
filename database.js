fb13cb0547af13bef99075c01ff6fd85b37f6e5e
alexanderasiamah363-del <alexanderasiamah363@gmail.com>
2026-03-30 03:31:01 +0000

    Create database.js

---
+++ database.js
@@ -0,0 +1,19 @@
+const universityDB = {
+    "UG": {
+        "name": "University of Ghana",
+        "programs": [
+            { "name": "BSc. Computer Science", "cutoff": 7 },
+            { "name": "BSc. Information Technology", "cutoff": 10 },
+            { "name": "Bachelor of Laws (LLB)", "cutoff": 7 },
+            { "name": "BSc. Administration", "cutoff": 9 }
+        ]
+    },
+    "KNUST": {
+        "name": "KNUST",
+        "programs": [
+            { "name": "BSc. Computer Science", "cutoff": 6 },
+            { "name": "BSc. Computer Engineering", "cutoff": 6 },
+            { "name": "BSc. Architecture", "cutoff": 12 }
+        ]
+    }
+};
